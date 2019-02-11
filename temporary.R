a1b493f77155a9e02576e7aee59cd486861d396a
IwoA <iwo_a@o2.pl>
2019-02-11 15:49:32 +0100

    First commit

---
+++ temporary.R
@@ -0,0 +1,15 @@
+# https://data.medicare.gov/data/hospital-compare
+
+# Kod Alaski to AK (https://pl.wikipedia.org/wiki/ISO_3166-2:US)
+
+tmp <- read.csv("https://data.medicare.gov/resource/k6j9-6siv.csv") #Complications and Deaths - State
+
+# Complications and deaths - state data. This data set includes state-level data for the hip/knee complication measure, 
+# the CMS Patient Safety Indicators, and 30-day death rates.
+
+
+# FRED All Employees: Health Care in Alaska
+# https://fred.stlouisfed.org/series/SMU02000006562001301A
+
+# All Employees: Education and Health Services: Hospitals in Alaska
+# https://fred.stlouisfed.org/series/SMU02000006562200001A
