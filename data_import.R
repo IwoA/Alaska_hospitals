92fa3a28b96a4a9c5243297be2e6d5eee3355a02
IwoA <iwo_a@o2.pl>
2019-02-26 12:50:23 +0100

    Code cleaning.

--- data_import.R
+++ data_import.R
@@ -1,3 +1,9 @@
+## Download and preprocess data about hospitals.
+##
+## Data about physicians are downloaded via physicians.R script.
+
+
+
 library(dplyr) 
 library(ggplot2)
 library(plotly)
@@ -50,52 +56,3 @@ colnames(hospitals)[1] <- "Values"
  write.csv(hospitals, "hospitals_perc.csv", row.names = FALSE)
 ### End of dataset in percentage
 
-
-
-
-#### Dataset in nominal values
-
-
-h2 <- h1
-h2 <- h2 %>% group_by(state) %>% count(Mortality)
-h2 <- group_by(state) %>% mutate(above = ifelse(Mortality=="Above the national average"), n, 0)
-
-h2 <- h2 %>%group_by(state) %>% 
-  mutate(n = n())
-
-for (i in 5:11) {  #correct order of levels
-  h2[[i]] <- factor(h2[[i]], levels(h1[[i]])[c(3,4,2,1)])
-}
-h2$Overall_rating <- factor(h2$Overall_rating, levels(h1$Overall_rating)[c(6, 1:5)]) #correct order of levels
- 
-
-
-### End of dataset in  nominal values
-
-
-#### Plot of hospital types, potentially sortable, in plotly
-g1 <- table(h2$state, h2$Type) %>% as.data.frame(.) %>% tidyr::spread( Var2, Freq)
-al <- filter(g1, Var1 == "AL")
-al$sum <- sum(al[2:4])
-a <- list(
-  y = al$Var1,
-  x = al$sum,
-  text = "AL",
-  xref = "x",
-  yref = "y",
-  showarrow = TRUE,
-  arrowhead = 1,
-  ax = 30,
-  ay = 0
-)
-
-library(plotly)
-plot_ly(g1, y = ~reorder(g1$Var1, g1$`Critical Access Hospitals`), x = g1$`Acute Care Hospitals`, type = "bar", orientation = 'h', name = 'Acute Care Hospitals') %>%
-  add_trace(x = ~g1$`Childrens`, name ="Childrens") %>% 
-  add_trace(x = ~g1$`Critical Access Hospitals`, name ="Critical Access Hospitals") %>% 
-  layout(xaxis = list(title = "No of hospitals", tickmode = "auto", nticks = 20), 
-         yaxis = list(title = "State", ticklen = 5, tickcolor = toRGB("white")), barmode = 'stack') %>%
-  layout(hovermode = 'compare') %>% 
-  layout(annotations = a)
-
-### End of plotly hospital types
